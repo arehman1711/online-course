8c33e848bd4949da67f3599daef6c524101c1681
Abdur Rehman <a.rehman1711@gmail.com>
2023-01-12 12:01:39 +0530

    Adding Logging

--- src/main/java/com/onlinecourse/OnlinecourseApplication.java
+++ src/main/java/com/onlinecourse/OnlinecourseApplication.java
@@ -3,11 +3,20 @@ package com.onlinecourse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class OnlinecourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinecourseApplication.class, args);
+		try {
+			log.info("Application Starting...");
+			SpringApplication.run(OnlinecourseApplication.class, args);
+			log.info("Application Started Successfully.");
+		} catch (Exception ex) {
+			log.error("Failed to start application" + ex.getMessage());
+		}
 	}
 
 }
--- src/main/java/com/onlinecourse/controller/HomeController.java
+++ src/main/java/com/onlinecourse/controller/HomeController.java
@@ -3,13 +3,18 @@ package com.onlinecourse.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
-public class HomeController {
 
+import lombok.extern.slf4j.Slf4j;
+
+
+@Controller
+@Slf4j
+public class HomeController {
 
 	@RequestMapping("/")
 	public String home(Model model) {
-	   model.addAttribute("title","Home-online Course Management");
+		model.addAttribute("title", "Home-online Course Management");
+		log.info("Enter in Home!");
 		return "home";
 	}
 }
