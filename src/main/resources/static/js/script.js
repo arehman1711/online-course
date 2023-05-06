8c7cebfeb37fa19fd7aad47f40e599152fa0999b
Abdur Rehman <a.rehman1711@gmail.com>
2023-05-06 14:07:43 +0530

    Add Character limit to Course Description and Char Count Display

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,3 +3,20 @@
          let form  = document.getElementById(id);;
             form.submit();
         }
+
+       $(document).ready(function() {
+       var text_max = $('#courseDescription').attr("maxlength");
+
+       var text_length = $('#courseDescription').val().length;
+       var text_remaining = text_max - text_length;
+
+       $('#textarea_feedback').html(text_max + ' characters remaining');
+
+       $('#courseDescription').keyup(function() {
+           var text_length = $('#courseDescription').val().length;
+           var text_remaining = text_max - text_length;
+
+           $('#textarea_feedback').html(text_remaining + ' characters remaining');
+       });
+
+       });
